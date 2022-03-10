49c24066caf75c11b460bc7aa7fb8a6ae1a1e093
Ebube Orazulume <m1906706@sgul.ac.uk>
2022-03-10 15:58:20 +0000

    not tested: updated script to make empty dirs and READAME.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,2 +1,7 @@
 !/usr/bin/env bash
+mkdir -p analysis docs data
 
+for my_directory in scripts analysis docs data;do
+ touch ${my_directory}/README.md
+ echo "# ${my_directory}" >> ${my_directory}/README.md
+done
